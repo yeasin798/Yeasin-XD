8a8813c474c81adffec6d437c804a73759133491
yeasin798 <yeasinarafat018933@gmail.com>
2025-02-14 14:43:15 +0600

    Update yeasin.py

--- yeasin.py
+++ yeasin.py
@@ -1,1 +1,8 @@
-
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+import yeasin
+yeasin.____Main___()
